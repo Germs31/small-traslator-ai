a1c6d5605660edec8d4543e93e6ada82009a95ee
Germs31 <german90nunez@gmail.com>
2024-12-03 21:04:36 -0800

    small css enchancement for textare and installed needed packages

--- src/components/TranslateBox/index.tsx
+++ src/components/TranslateBox/index.tsx
@@ -3,7 +3,11 @@ import { ITranslate } from './types'
 
 const TranslateBox = (props: ITranslate) => {
   return (
-    <textarea name={props.name} id={props.name}></textarea>
+    <textarea 
+      name={props.name} 
+      id={props.name}
+      rows={5}
+      className="py-2.5 px-4 border-none focus:outline-none block w-full border-transparent rounded-lg dark:bg-neutral-900 dark:border-transparent dark:text-neutral-400"></textarea>
   )
 }
 
